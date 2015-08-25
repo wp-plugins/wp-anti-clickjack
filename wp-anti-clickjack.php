e3ac51e1847d6fd2b7220e94def17a35dba675ba
someguy9 <someguy9@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-25 15:11:49 +0000

    Tagging version 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-anti-clickjack/tags/1.1.0@1230373 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-anti-clickjack.php
+++ wp-anti-clickjack.php
@@ -3,42 +3,43 @@
  * Plugin Name: WP Anti-Clickjack
  * Plugin URI: http://drawne.com/wordpress-anti-clickjack-plugin/
  * Description: Plugin to prevent your site from being clickjacked
- * Version: 1.0.0
+ * Version: 1.1.0
  * Text Domain: wp-anti-clickjack
  * Author: Andy Feliciotti
  * Author URI: http://drawne.com
  */
  
-defined('ABSPATH') or die("you do not have acces to this page!");
+defined('ABSPATH') or die("you do not have access to this page!");
+
 
 class wp_anticlickjack {
 
     public function __construct()
     {
-        add_action('wp_head', array($this,'include_anticlickjack_script'));
+			add_action('wp_head', array($this,'include_anticlickjack_script'));
     }
 
     public function include_anticlickjack_script() {
-        ?>
-  <script language="javascript" type="text/javascript">
-	 var style = document.createElement('style');
-	 style.type = "text/css";
-	 style.id = "antiClickjack";
-	 if ('cssText' in style){
-	   style.cssText = "body{display:none !important;}";
-	 }else{
-	   style.innerHTML = "body{display:none !important;}";
-	}
-	document.getElementsByTagName('head')[0].appendChild(style);
-
-	if (self === top) {
-	 var antiClickjack = document.getElementById("antiClickjack");
-	 antiClickjack.parentNode.removeChild(antiClickjack);
-	} else {
-	 top.location = self.location;
-	}
-  </script>
-        <?php
+	    if ( !is_customize_preview() ) {
+	        echo '<script language="javascript" type="text/javascript">
+			 var style = document.createElement("style");
+			 style.type = "text/css";
+			 style.id = "antiClickjack";
+			 if ("cssText" in style){
+			   style.cssText = "body{display:none !important;}";
+			 }else{
+			   style.innerHTML = "body{display:none !important;}";
+			}
+			document.getElementsByTagName("head")[0].appendChild(style);
+		
+			if (self === top) {
+			 var antiClickjack = document.getElementById("antiClickjack");
+			 antiClickjack.parentNode.removeChild(antiClickjack);
+			} else {
+			 top.location = self.location;
+			}
+		  </script>';
+		}
     }
 }
 
